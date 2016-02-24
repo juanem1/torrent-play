5a743aaad9e4c4ee347bf2b5b9bd390e3986cc54
Juan Emilio Turk <juan.turk@globallogic.com>
2016-02-24 12:09:52 -0300

    added params gulp module add posibility to pass param in the gult start task in order to "start" in different operating systems

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,38 @@
-var gulp = require('gulp');
+var gulp = require('gulp-param')(require('gulp'), process.argv);
 var exec = require('gulp-exec');
 var builder = require('gulp-nw-builder');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
 var fs = require('fs');
 
-
-var BUILD_VERSION = 'v0.12.2';
+/**
+ * Available platforms:
+ * win32, win64, osx32, osx64, linux32, linux64
+ * @type {string[]}
+ */
+var BUILD_PLATFORMS = ['osx64', 'linux64'];
+var BUILD_VERSION   = 'v0.12.2';
 
 
 /**
  * Start task
- * Run: gulp start
+ * By default will start osx64
+ * Run: gulp start --platform linux64
  */
-gulp.task('start', function() {
+gulp.task('start', function(platform) {
+
+    // Start path for all OS's
     var paths = {
-        osx32: 'build/torrentPlay/osx32/torrentPlay.app/Contents/MacOS/nwjs .',
-        osx64: 'build/torrentPlay/osx64/torrentPlay.app/Contents/MacOS/nwjs .',
-        win32: 'build/torrentPlay/win32/torrentPlay.exe .',
-        win64: 'build/torrentPlay/win64/torrentPlay.exe .'
+        osx32:   'build/torrentPlay/osx32/torrentPlay.app/Contents/MacOS/nwjs .',
+        osx64:   'build/torrentPlay/osx64/torrentPlay.app/Contents/MacOS/nwjs .',
+        win32:   'build/torrentPlay/win32/torrentPlay.exe .',
+        win64:   'build/torrentPlay/win64/torrentPlay.exe .',
+        linux32: 'build/torrentPlay/linux32/torrentPlay .',
+        linux64: 'build/torrentPlay/linux64/torrentPlay .'
     };
+
+    // Default start path
+    var platformPath = paths.osx64;
 
     var options = {
         // default = false, true means don't emit error event
@@ -43,8 +56,36 @@ gulp.task('start', function() {
         stdout: true
     };
 
+    if (platform) {
+        switch (platform) {
+            case 'osx32':
+                platformPath = paths.osx32;
+                break;
+
+            case 'win32':
+                platformPath = paths.win32;
+                break;
+
+            case 'win64':
+                platformPath = paths.win64;
+                break;
+
+            case 'linux32':
+                platformPath = paths.linux32;
+                break;
+
+            case 'linux64':
+                platformPath = paths.linux64;
+                break;
+
+            default:
+                platformPath = paths.osx64;
+                break;
+        }
+    }
+
     gulp.src('./src')
-        .pipe(exec(paths.osx64, options))
+        .pipe(exec(platformPath, options))
         .pipe(exec.reporter(reportOptions));
 });
 
@@ -82,7 +123,7 @@ gulp.task('build:make', function() {
     ]).pipe(
         builder({
             version: BUILD_VERSION,
-            platforms: ['osx64']
+            platforms: BUILD_PLATFORMS
         })
     );
 });
